de259682c1f27f61d1bf5189ee7fcb4d3d277141
Athira Parameswaran s <athira007p@gmail.com>
2023-03-20 09:35:26 +0530

    New commits

--- src/Acme.BookStore.Application/BookStoreApplicationAutoMapperProfile.cs
+++ src/Acme.BookStore.Application/BookStoreApplicationAutoMapperProfile.cs
@@ -1,4 +1,5 @@
-﻿using AutoMapper;
+﻿using Acme.BookStore.Books;
+using AutoMapper;
 
 namespace Acme.BookStore;
 
@@ -6,6 +7,9 @@ public class BookStoreApplicationAutoMapperProfile : Profile
 {
     public BookStoreApplicationAutoMapperProfile()
     {
+
+        CreateMap<Book, BookDto>();
+        CreateMap<CreateUpdateBookDto, Book>();
         /* You can configure your AutoMapper mapping configuration here.
          * Alternatively, you can split your mapping configurations
          * into multiple profile classes for a better organization. */
